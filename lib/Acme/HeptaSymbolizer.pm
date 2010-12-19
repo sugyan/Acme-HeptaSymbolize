9f86da48c7a83cefd4d870a9d73456abb57e508c
sugyan <sugi1982@gmail.com>
2010-12-20 01:18:25 +0900

    initial commit

---
+++ lib/Acme/HeptaSymbolizer.pm
@@ -0,0 +1,32 @@
+package Acme::HeptaSymbolizer;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::HeptaSymbolizer -
+
+=head1 SYNOPSIS
+
+  use Acme::HeptaSymbolizer;
+
+=head1 DESCRIPTION
+
+Acme::HeptaSymbolizer is
+
+=head1 AUTHOR
+
+sugyan E<lt>sugi1982@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
